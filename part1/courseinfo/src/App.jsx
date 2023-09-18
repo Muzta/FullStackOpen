9e7e6f28939100c94871c0c077fb3cd9e55ccf1b
muzta <javyngs@gmail.com>
2023-09-18 13:31:02 +0200

    Finished 'course information, step 4'

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -27,24 +27,32 @@ const Content = (props) => {
 };
 
 const Total = (props) => {
+  console.log("Total ", props);
   return (
     <>
-      <p>Number of exercises {props.exercises}</p>
+      <p>
+        Number of exercises{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
+      </p>
     </>
   );
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = { name: "Fundamentls of React", exercises: 10 };
-  const part2 = { name: "Using props to pass data", exercises: 7 };
-  const part3 = { name: "State of a component", exercises: 14 };
+  const parts = [
+    { name: "Fundamentls of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total exercises={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
