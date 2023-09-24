691fac723a4ffb2d9a994b80e30692d76691e280
muzta <javyngs@gmail.com>
2023-09-24 13:08:56 +0200

    Finished part 2 'course information, step 7'

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -6,6 +6,12 @@ const Content = ({ parts }) => {
       {parts.map((part) => (
         <Part key={part.id} part={part} />
       ))}
+      <p>
+        <b>
+          Total of {parts.reduce((sum, part) => sum + part.exercises, 0)}{" "}
+          exercises
+        </b>
+      </p>
     </div>
   );
 };
