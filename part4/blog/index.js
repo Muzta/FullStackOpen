18c4a67e0adeeee21143359a9ababdbb22edcc5d
muzta <javyngs@gmail.com>
2023-10-10 16:56:41 +0200

    Created controllers for ex 4.2

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+const blogsRouter = require("./controllers/blogs");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
 
@@ -12,20 +12,7 @@ mongoose.connect(mongoUrl);
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
