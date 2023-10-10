d87e0c13674f9d025138d9df74dc985a75753a16
muzta <javyngs@gmail.com>
2023-10-10 17:09:48 +0200

    Created app file and finished the ex 4.2

---
+++ part4/blog/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+
+mongoose
+  .connect(config.MONGO_URI)
+  .then(() => logger.info("Connected to MongoDB"))
+  .catch((error) => logger.error("Error connecting to MongoDB", error.message));
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,19 +1,10 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const blogsRouter = require("./controllers/blogs");
+const app = require("./app");
+const http = require("http");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
 
-const mongoUrl = config.MONGO_URI;
-mongoose.connect(mongoUrl);
+const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json());
-app.use("/api/blogs", blogsRouter);
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
