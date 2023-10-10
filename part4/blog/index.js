3363efb59d961a32f5a41ecf8fb846c65f4db1b7
muzta <javyngs@gmail.com>
2023-10-10 16:51:49 +0200

    Created models for ex 4.2

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -3,17 +3,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
 
 const mongoUrl = config.MONGO_URI;
 mongoose.connect(mongoUrl);
