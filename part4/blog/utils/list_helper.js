f0b1a51952a0794f0c28f2c3195f500594567099
muzta <javyngs@gmail.com>
2023-10-11 18:36:33 +0200

    Finished ex 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,4 +6,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+  const blog = blogs.reduce(
+    (currentMax, blog) => (blog.likes > currentMax.likes ? blog : currentMax),
+    blogs[0]
+  );
+  const { title, author, likes } = blog;
+  console.log("Propiedades", { title, author, likes });
+  return { title, author, likes };
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
