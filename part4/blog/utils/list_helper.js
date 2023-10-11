daaeaf9e9bac748d2669d7f154540b73bd2b165d
muzta <javyngs@gmail.com>
2023-10-11 19:22:12 +0200

    Finished ex 4.6

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require("lodash");
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -8,13 +10,27 @@ const totalLikes = (blogs) => {
 
 const favoriteBlog = (blogs) => {
   if (blogs.length === 0) return null;
+
   const blog = blogs.reduce(
     (currentMax, blog) => (blog.likes > currentMax.likes ? blog : currentMax),
     blogs[0]
   );
   const { title, author, likes } = blog;
   console.log("Propiedades", { title, author, likes });
+
   return { title, author, likes };
 };
 
-module.exports = { dummy, totalLikes, favoriteBlog };
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  const blogsPerAuthor = lodash.countBy(blogs, "author");
+  //   Make a list of pairs Author: blogs, and get the object with the highest [1] (num of blogs) property
+  const topAuthor = lodash.maxBy(
+    lodash.toPairs(blogsPerAuthor),
+    (blog) => blog[1]
+  );
+  return { author: topAuthor[0], blogs: topAuthor[1] };
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs };
