3dd6e3f43e0d86a0b4f0b1af3af250388f414a8f
muzta <javyngs@gmail.com>
2023-10-11 17:59:05 +0200

    Finished ex 4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,4 +2,8 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+module.exports = { dummy, totalLikes };
