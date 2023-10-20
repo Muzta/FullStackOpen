33f6c55c32fa066a36b59c378d637741da909514
muzta <javyngs@gmail.com>
2023-10-20 13:58:27 +0200

    Finished middleware ex 4.22

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,14 +1,13 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
-const User = require("../models/user");
-const jwt = require("jsonwebtoken");
+const middleware = require("../utils/middleware");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response) => {
+blogsRouter.post("/", middleware.userExtractor, async (request, response) => {
   const body = request.body;
   if (!body.title || !body.url)
     return response.status(400).json({ error: "Title or url missed" });
@@ -23,23 +22,25 @@ blogsRouter.post("/", async (request, response) => {
   response.status(201).json(result);
 });
 
-blogsRouter.delete("/:id", async (request, response) => {
-  const blogId = request.params.id;
-  const userToken = jwt.verify(request.token, process.env.SECRET);
-  if (!userToken.id)
-    return response.status(401).json({ error: "Invalid token" });
+blogsRouter.delete(
+  "/:id",
+  middleware.userExtractor,
+  async (request, response) => {
+    const blogId = request.params.id;
+    const blog = await Blog.findById(blogId);
+    if (!blog) return response.status(404).json({ error: "Blog not found" });
 
-  const blog = await Blog.findById(blogId);
-  if (!blog) return response.status(404).json({ error: "Blog not found" });
+    const user = request.user;
 
-  if (blog.user.toString() !== userToken.id)
-    return response
-      .status(401)
-      .json({ error: "Only blog owner can delete it" });
+    if (blog.user.toString() !== user.id)
+      return response
+        .status(401)
+        .json({ error: "Only blog owner can delete it" });
 
-  await blog.deleteOne();
-  return response.status(204).end();
-});
+    await blog.deleteOne();
+    return response.status(204).end();
+  }
+);
 
 blogsRouter.put("/:id", async (request, response) => {
   const id = request.params.id;
