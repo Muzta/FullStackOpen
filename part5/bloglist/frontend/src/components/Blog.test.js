906f4952e25ef3cca74d5bd1edfc4df33b3d7592
muzta <javyngs@gmail.com>
2023-11-03 16:55:48 +0100

    Finished bloglist tests ex 5.14

--- part5/bloglist/frontend/src/components/Blog.test.js
+++ part5/bloglist/frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
 describe("<Blog />", () => {
@@ -15,19 +16,48 @@ describe("<Blog />", () => {
     likes: 100,
     user: user,
   };
-  let blogRendered;
+  let component;
 
   beforeEach(() => {
-    blogRendered = render(<Blog blog={blog} />);
+    component = render(<Blog blog={blog} />);
   });
 
   test("Displaying a blog only renders its title and author by default", () => {
-    const blogData = blogRendered.container.querySelector("#blog-data");
-    const blogContent = blogRendered.container.querySelector("#blog-content");
-    expect(blogData).toHaveTextContent(
-      "Component testing is done with react-testing-library"
-    );
+    const blogData = component.container.querySelector("#blog-data");
+    const blogContent = component.container.querySelector("#blog-content");
+
+    expect(blogData).toHaveTextContent(blog.title);
     expect(blogData).not.toHaveTextContent("http://test.com");
     expect(blogContent).toBeNull();
+  });
+
+  test("Url and likes are shown when toggle button is clicked", async () => {
+    expect(component.container).not.toHaveTextContent(blog.url);
+    expect(component.container).not.toHaveTextContent(`Likes ${blog.likes}`);
+
+    const user = userEvent.setup();
+    const viewButton = screen.getByText("View");
+    await user.click(viewButton);
+
+    expect(component.container).toHaveTextContent(blog.url);
+    expect(component.container).toHaveTextContent(`Likes ${blog.likes}`);
+  });
+
+  test("Url and likes are hidden when toggle button is clicked twice", async () => {
+    expect(component.container).not.toHaveTextContent(blog.url);
+    expect(component.container).not.toHaveTextContent(`Likes ${blog.likes}`);
+
+    const user = userEvent.setup();
+    const viewButton = screen.getByText("View");
+    await user.click(viewButton);
+
+    expect(component.container).toHaveTextContent(blog.url);
+    expect(component.container).toHaveTextContent(`Likes ${blog.likes}`);
+
+    const hideButton = screen.getByText("Hide");
+    await user.click(hideButton);
+
+    expect(component.container).not.toHaveTextContent(blog.url);
+    expect(component.container).not.toHaveTextContent(`Likes ${blog.likes}`);
   });
 });
