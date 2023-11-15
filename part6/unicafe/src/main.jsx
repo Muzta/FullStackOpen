40c5c040559cb128e24c9337d73e2dd4f6215949
muzta <javyngs@gmail.com>
2023-11-15 12:06:36 +0100

    Finished unicafe revisited ex 6.2

--- part6/unicafe/src/main.jsx
+++ part6/unicafe/src/main.jsx
@@ -1,36 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { createStore } from 'redux'
-import reducer from './reducer'
+import { createStore } from "redux";
+import reducer from "./reducers/reducer";
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+  const good = () => store.dispatch({ type: "GOOD" });
+  const ok = () => store.dispatch({ type: "OK" });
+  const bad = () => store.dispatch({ type: "BAD" });
+  const reset = () => store.dispatch({ type: "ZERO" });
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <button onClick={good}>Good</button>
+      <button onClick={ok}>Ok</button>
+      <button onClick={bad}>Bad</button>
+      <button onClick={reset}>Reset stats</button>
+      <div>Good {store.getState().good}</div>
+      <div>Ok {store.getState().ok}</div>
+      <div>Bad {store.getState().bad}</div>
     </div>
-  )
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const renderApp = () => {
-  root.render(<App />)
-}
+  root.render(<App />);
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
