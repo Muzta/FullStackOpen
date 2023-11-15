2d45ff4159f947389cea23f5560c142e4cce1c48
muzta <javyngs@gmail.com>
2023-11-15 17:54:14 +0100

    Finished anecdotes ex 6.5
    Anecdotes are ordered by the number of votes

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,7 +2,9 @@ import { useSelector, useDispatch } from "react-redux";
 import { voteAnecdote, createAnecdote } from "./reducers/anecdoteReducer";
 
 const App = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector((state) =>
+    state.sort((a1, a2) => a2.votes - a1.votes)
+  );
   const dispatch = useDispatch();
 
   const vote = (id) => {
