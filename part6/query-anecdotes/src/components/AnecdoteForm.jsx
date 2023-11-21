8df83032ac7a8157f0775dae7f87997851140583
muzta <javyngs@gmail.com>
2023-11-21 11:49:52 +0100

    Finished anecdotes ex 6.21
    Implemented adding new anecdotes using React Query. There's not error handling yet

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,9 +1,23 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createAnecdote } from "../requests";
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient();
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData(["anecdotes"]);
+      queryClient.setQueryData(["anecdotes"], anecdotes.concat(newAnecdote));
+    },
+  });
+
   const onCreate = (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = "";
     console.log("new anecdote");
+    newAnecdoteMutation.mutate({ content, votes: 0 });
   };
 
   return (
--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -4,3 +4,6 @@ const baseUrl = "http://localhost:3001/anecdotes";
 
 export const getAnecdotes = () =>
   axios.get(baseUrl).then((response) => response.data);
+
+export const createAnecdote = (newAnecdote) =>
+  axios.post(baseUrl, newAnecdote).then((response) => response.data);
