0c0854389d13713fcffdfb393b7ea494e2961f07
muzta <javyngs@gmail.com>
2023-11-23 12:45:01 +0100

    Finished country hook ex 7.7
    Implemented a custom hook to fetch countries data if the searched name exists in the API

---
+++ part7/country-hook/src/App.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return { type, value, onChange };
+};
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    if (name) {
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+        .then((response) => setCountry(response.data))
+        .catch((error) => setCountry(error.response.data));
+    }
+  }, [name]);
+
+  return country;
+};
+
+const Country = ({ country }) => {
+  if (!country) return null;
+
+  if (country.error) return <div>not found...</div>;
+
+  return (
+    <div>
+      <h3>{country.name.common} </h3>
+      <div>capital {country.capital[0]} </div>
+      <div>population {country.population}</div>
+      <img
+        src={country.flags.svg}
+        height="100"
+        alt={`flag of ${country.name.common}`}
+      />
+    </div>
+  );
+};
+
+const App = () => {
+  const nameInput = useField("text");
+  const [name, setName] = useState("");
+  const country = useCountry(name);
+
+  const fetch = (e) => {
+    e.preventDefault();
+    setName(nameInput.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  );
+};
+
+export default App;
