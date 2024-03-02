f3b7f0aaedeafa35486f6850c33b55bfe9a3e59d
muzta <javyngs@gmail.com>
2024-03-02 16:57:17 +0100

    Remove part8/library-frontend submodule reference

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,36 @@
+import { useQuery } from "@apollo/client";
+import { ALL_AUTHORS } from "../queries";
+
+const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS);
+
+  if (!props.show) return null;
+
+  if (result.loading) return <div>Loading...</div>;
+
+  const authors = result.data.allAuthors;
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.id}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Authors;
