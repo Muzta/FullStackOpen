e02ba353ae0b05109861dc40adaaaf28cf587b6d
muzta <javyngs@gmail.com>
2024-03-29 18:09:36 +0100

    Finished part 8 ex 8.19
    Books can be now filtered by genre

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,18 +1,41 @@
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
+import Select from "react-select";
+import { useState } from "react";
 
 const Books = (props) => {
-  const result = useQuery(ALL_BOOKS);
+  const [selectedGenre, setSelectedGenre] = useState("");
+  const { loading, data } = useQuery(ALL_BOOKS);
 
   if (!props.show) return null;
+  if (loading) return <div>Loading...</div>;
 
-  if (result.loading) return <div>Loading...</div>;
+  const books = data.allBooks;
 
-  const books = result.data.allBooks;
+  const removeDuplicates = (array) => [...new Set(array)];
+
+  const booksToShow = selectedGenre
+    ? books.filter((book) => book.genres.includes(selectedGenre))
+    : books;
+
+  const allGenres = removeDuplicates(books.flatMap((book) => book.genres));
+
+  const genreSelectOptions = [
+    { value: "", label: "All genres" },
+    ...allGenres.map((genre) => ({ value: genre, label: genre })),
+  ];
 
   return (
     <div>
       <h2>books</h2>
+
+      <Select
+        required
+        isSearchable={true}
+        options={genreSelectOptions}
+        onChange={(selectedOption) => setSelectedGenre(selectedOption.value)}
+        defaultValue={genreSelectOptions[0]}
+      />
 
       {books.length !== 0 && (
         <table>
@@ -20,12 +43,14 @@ const Books = (props) => {
             <tr>
               <th></th>
               <th>author</th>
+              <th>genres</th>
               <th>published</th>
             </tr>
-            {books.map((a) => (
+            {booksToShow.map((a) => (
               <tr key={a.title}>
                 <td>{a.title}</td>
                 <td>{a.author.name}</td>
+                <td>{a.genres.join(", ")}</td>
                 <td>{a.published}</td>
               </tr>
             ))}
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -12,13 +12,14 @@ export const ALL_AUTHORS = gql`
 `;
 
 export const ALL_BOOKS = gql`
-  query {
-    allBooks {
+  query ($genre: String) {
+    allBooks(genre: $genre) {
       title
       published
       author {
         name
       }
+      genres
       id
     }
   }
