df77416665f44001f580807c6e97cc339433d9f4
muzta <javyngs@gmail.com>
2024-04-12 12:18:15 +0200

    Finished part 9 ex 9.9
    Forked and clone the project patientor frontend

--- part9/patientor/backend/index.ts
+++ part9/patientor/backend/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import cors from "cors";
 const app = express();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3000;
